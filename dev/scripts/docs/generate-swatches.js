05137dc1ecb3fff189d3052f53f7b52244696203
Denis Kabana <8308865+deniskabana@users.noreply.github.com>
2025-06-05 11:47:42 +0200

    Improved swatch generation

---
+++ dev/scripts/docs/generate-swatches.js
@@ -0,0 +1,42 @@
+const fs = require("fs");
+const path = require("path");
+
+// ==================================================
+// IMPLEMENTATION
+// ==================================================
+
+const name = "generate-swatches";
+
+async function init() {
+  const palette = require(path.join(__dirname, "../../../palette.dark.json"));
+  const outputDir = path.join(__dirname, "../../../assets/swatches/");
+
+  // Generate SVG
+  const size = 28;
+  const radius = 14;
+  const borderColor = "#666666"
+
+  palette.forEach(color => {
+    let svg = `<svg width="${size}" height="${size}" xmlns="http://www.w3.org/2000/svg">\n`;
+    svg += `  <circle cx="${radius}" cy="${radius}" r="${radius}" fill="${color.hex}" stroke="${borderColor}" stroke-width="1" />\n`;
+    svg += `</svg>`;
+    fs.writeFileSync(path.join(outputDir, `${color.name}_dark.svg`), svg);
+  });
+
+  return `${palette.length} swatches created in ${outputDir}`;
+}
+
+// ==================================================
+// EXECUTION
+// ==================================================
+
+(async function () {
+  const isVerbose = process.argv.includes("--verbose");
+
+  try {
+    console.log(`[${name}] ✅ Success!`, await init());
+  } catch (e) {
+    if (isVerbose) console.error(`[${name}] ❌ Error!`, e);
+    else console.error(`[${name}] ❌ Error! Use --verbose flag for details.`);
+  }
+})()
